1e05e0f9581130b3b15623ec24c1e9212adb4afc
Malcolm Diller <dillerm@oregonstate.edu>
2016-01-25 15:50:22 -0800

    weweb: added editing functionality to groups and lights

--- web/start.py
+++ web/start.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import random as rand
 import unicodedata,socket
 from isc_dhcp_leases.iscdhcpleases import Lease, IscDhcpLeases
@@ -72,6 +73,26 @@ AVAILABLE_COMMANDS = {
     '3 OFF': '01101',
     '4 OFF': '01110',
 }
+
+# Returns a linear list of all of the groups within a nested list
+def getGroups(members):
+    result = []
+    for member in members:
+        if type(member) is not Light:
+            result.append(member)
+            result = result + getGroups(member.members)
+    return result
+
+# Returns a linear list of all of the lights within a group of lights or groups
+def getLights(members):
+    result = []
+    for member in members:
+        if type(member) is Light:
+            result.append(member)
+        else:
+            result = result + getLights(member.members)
+    return result
+
 # This function should also be a member of a lights class too
 def enlighten(cmd,mac):
     new_state = ""
@@ -147,7 +168,7 @@ def devices_search():
     # For each lease, first see if it's an ESP8266
     for iteration,lease in enumerate(leases):
         if(ESP8266_check(lease.ip)):
-            # We have an ESP8266 module!
+            # We have an ESP8266 mode!
             database_check = model.Device.query.filter_by(mac=lease.ethernet).first()
             if database_check is None:
             # We have an ESP8266 AND it's not in the database!
@@ -156,6 +177,27 @@ def devices_search():
                 model.db.session.commit()
                 additions = additions + 1
     return str(additions)
+
+@app.route('/change_name', methods=['POST'])
+def change_name():
+    name = request.form['name']
+    id = request.form['id']
+    if "|" in id:
+        # This is a Light
+        mac = id.split("|")[0]
+        port = int(id.split("|")[1])
+        for light in getLights(groups):
+            if light.mac == mac and light.port == port:
+                light.name = name
+    else:
+        # This is a Group
+        gid = int(id)
+        for group in getGroups(groups):
+            if group.gid == gid:
+                group.name = name
+
+
+    return "SUCCESSFUL THINGS WERE DONE"
 
 def ESP8266_check(ipaddr):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
