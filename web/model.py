f0241a31438c038dcf24d63b145199db89fe8bce
Evan Steele <esteele101@gmail.com>
2016-01-28 09:45:44 -0800

    web: added some more tables

--- web/model.py
+++ web/model.py
@@ -30,3 +30,21 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+
+class Group(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    GroupID = db.Column(db.Integer, db.ForeignKey('group.id'))
+    name = db.Column(db.String(128))
+
+class Light(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    parent = db.Column(db.Integer, db.ForeignKey('group.id'))
+    name = db.Column(db.String(128))
+    device = db.Column(db.String(12), db.ForeignKey('device.id'))
+    port = db.Column(db.Integer)
+
+class Rules(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    string = db.Column(db.String(256))
+    lightID = db.Column(db.Integer, db.ForeignKey('light.id'))
+    groupID = db.Column(db.Integer, db.ForeignKey('group.id'))
