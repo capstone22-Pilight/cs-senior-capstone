190cd638a0a3a3fb218abee1d47fd775b45a943e
Evan Steele <esteele101@gmail.com>
2016-04-22 11:47:17 -0700

    web: finished poller

--- web/static/js/poll.js
+++ web/static/js/poll.js
@@ -1,11 +1,24 @@
 setInterval(function(){
    var numdevices = 5;
+   var state;
    $.ajax({
       url: "/poll",
       global: false,
       type: "POST",
       cache: false,
       success: function(response){
-         console.log(response);
+         for (var key in response) {
+            if (response.hasOwnProperty(key)) {
+               switch_button = $("li[lid='" + key + "']").find('input');
+               if (response[key] == '0'){
+                  state = false;
+               }else if (response[key] == '1'){
+                  state = true;
+               }
+               console.log(state);
+               switch_button.bootstrapSwitch('state',state);
+               state = null;
+            }
+         }
       }});
 },3000);
